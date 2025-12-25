5bfc61b9a462a057fbc3b2524eba10d622e5b4af
Roland PJ <rolandpj@gmail.com>
2025-12-25 22:16:50 +0200

    introduce and null plumb LexicalScope; no functional change

---
+++ include/natalie/lexical_scope.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Natalie {
+class ModuleObject;
+
+class LexicalScope {
+public:
+    LexicalScope(LexicalScope *parent, ModuleObject *module)
+        : m_parent(parent)
+        , m_module(module) { }
+
+    LexicalScope *parent() const { return m_parent; }
+    ModuleObject *module() const { return m_module; }
+
+private:
+    LexicalScope *m_parent { nullptr };
+    ModuleObject *m_module { nullptr };
+};
+}
